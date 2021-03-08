76b440cb239c183afbc30bc93b18d7e454801f07
Timur <timka_issaev@mail.ru>
2021-03-08 20:11:25 +0300

    improved

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  define('SITE_ROOT', str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']).'/');
+  define('TITLE', 'Главная');
+  include SITE_ROOT.'libs/top_vars.php';
+?>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html>
+<?php
+  include SITE_ROOT.'templates/header.php';
+?>
+<head>
+	
+</head>
+
+<body>
+	
+</body>
+
+</html>
