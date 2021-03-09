1c302de8dde775716ef1008d3ce0c7c99d635dca
Timur <timka_issaev@mail.ru>
2021-03-09 14:15:19 +0300

    footer template is created

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+	<div>
+		Сайт разработан специально для конкурса "Цифровой ветер - 2021" разработчиком Исаевым Тимуром
+	</div>
+  <div>
+  	2021 ©  Аля гурме 
+  </div>
+
+  <div style="text-align: center;width:143px;margin:17px auto;">
+    <a href="//www.digitalwind.ru" target="_blank">
+    <img id="bxid_862078" src="http://www.digitalwind.ru/upload/2021/лого2021.png" title="www.digitalwind.ru" border="0" align="left" alt="www.digitalwind.ru" width="143" height="51">
+</a>
+
+
+  </div>
+</div>
