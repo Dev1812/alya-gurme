9ba3400aa74623853aa765761c455a7fbe8e1852
Timur <timka_issaev@mail.ru>
2021-03-09 18:22:03 +0300

    fixed bugs

--- templates/head.php
+++ templates/head.php
@@ -1,37 +1,3 @@
-<style type="text/css">
-	@font-face{font-family:Myriad_set_pro_Thin;src:local(Myriad_set_pro_Thin),url(../fonts/myriad-set-pro_text.woff)}
-*{
-  box-sizing:border-box;
-  font-family: -apple-system ,BlinkMacSystemFont ,'Segoe UI', "Myriad_set_pro_Thin","SF Pro Text","Myriad Set Pro","SF Pro Icons","Apple Legacy Chevron","Helvetica Neue","Helvetica","Arial",sans-serif;
-}
-
-
-.head{height:53px;padding:0 21px;border-bottom:1px solid #DDDFE1;background-color: #FFF;}
-.float_left{float:left;}
-.float_right{float:right;}
-.head-link{float:left;padding:15px 11px 15px;}
-.head-logo{position: relative;right:7px;top: 0px;}
-html, body{font-size:15px;margin:0;padding:0;}
-a{text-decoration: none;color: #607d8b}
-.text_field{border:1px solid #DDD;background-color: #F7F9FB;
-
-    background-color: #F7F9FB;
-    padding: 9px 9px 9px 31px;
-    margin-top: 11px;
-    background-image: url('/images/icons/magnify (3).png');
-    background-repeat: no-repeat;
-    background-position: 8px 4px;
-
-
-    border: 1px solid #DDD;
-    width: 100%;
-    height: 7px;
-    line-height: 24px;
-
-border-radius:3px;
-    }
-</style>
-
 <div class="head">
   <div class="head-wrap">
   	<div class="head-left_section float_left">
@@ -44,16 +10,14 @@ border-radius:3px;
   	</div>
     
 
-      <div class="head-left_section float_left" style="width:140px;margin-left:14px;">
+      <div class="head-left_section float_left" style="width:190px;margin-left:14px;">
       	<FORM action="">
       		
       	</FORM>
        <input type="text" name="q" class="text_field" placeholder="Поиск">
       </div>
 
-      
-
-      <div style="width:143px;margin:0 auto;">
+      <div class="head-center">
       	
       	<a href="//www.digitalwind.ru" target="_blank">
     <img id="bxid_862078" src="http://www.digitalwind.ru/upload/2021/лого2021.png" title="www.digitalwind.ru" border="0" align="left" alt="www.digitalwind.ru" width="143" height="51">
@@ -61,13 +25,11 @@ border-radius:3px;
 
       </div>
 
-
   	<div class="head-right_section float_right">
   	  <a href="/login.php" class="head-link">Админ-панель</a>
   	  <a href="/login.php" class="head-link">Войти</a>
   	  <a href="/reg.php" class="head-link">Регистрация</a>
   	</div>
-
 
   </div>
 </div>
