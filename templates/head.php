62671fe503d4a5fc3e1809028e36806c99d17d0d
Timur <timka_issaev@mail.ru>
2021-03-09 14:14:44 +0300

    head of page is created

--- templates/head.php
+++ templates/head.php
@@ -1,11 +1,28 @@
 <style type="text/css">
-.head{height:51px;padding:0 11px;}
+.head{height:53px;padding:0 21px;border-bottom:1px solid #DDDFE1;}
 .float_left{float:left;}
 .float_right{float:right;}
 .head-link{float:left;padding:15px 11px 15px;}
 .head-logo{position: relative;right:7px;top: 0px;}
-html, body{margin:0;}
+html, body{margin:0;font:14px "Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, Verdana, sans-serif;}
 a{text-decoration: none;color: #607d8b}
+.text_field{border:1px solid #DDD;background-color: #F7F9FB;
+
+    background-color: #F7F9FB;
+    padding: 9px 9px 9px 31px;
+    margin-top: 11px;
+    background-image: url('/images/icons/magnify (3).png');
+    background-repeat: no-repeat;
+    background-position: 8px 4px;
+
+
+    border: 1px solid #DDD;
+    width: 100%;
+    height: 7px;
+    line-height: 24px;
+
+border-radius:3px;
+    }
 </style>
 
 <div class="head">
@@ -20,14 +37,28 @@ a{text-decoration: none;color: #607d8b}
   	</div>
     
 
-      <div class="head-left_section" style="top:17px;position:relative;">
-       <input type="text" name="">
+      <div class="head-left_section float_left" style="width:140px;margin-left:14px;">
+      	<FORM action="">
+      		
+      	</FORM>
+       <input type="text" name="q" class="text_field" placeholder="Поиск">
+      </div>
+
+      <div style="width:143px;margin:0 auto;">
+      	
+      	<a href="//www.digitalwind.ru" target="_blank">
+    <img id="bxid_862078" src="http://www.digitalwind.ru/upload/2021/лого2021.png" title="www.digitalwind.ru" border="0" align="left" alt="www.digitalwind.ru" width="143" height="51">
+</a>
+
       </div>
 
 
   	<div class="head-right_section float_right">
-  	  <a href="/" class="head-link">Войти</a>
-  	  <a href="/" class="head-link">Регистрация</a>
+  	  <a href="/login.php" class="head-link">Админ-панель</a>
+  	  <a href="/login.php" class="head-link">Войти</a>
+  	  <a href="/reg.php" class="head-link">Регистрация</a>
   	</div>
+
+
   </div>
 </div>
